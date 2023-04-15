8658fe24777045512ee4e7c83dd50588aafe54a3
probitiye <vm683497580@gmail.com>
2023-04-15 21:36:05 +0300

    moving forward/back/left/right

--- event_system_t.cpp
+++ event_system_t.cpp
@@ -16,11 +16,24 @@ bool event_system_t::should_close_app()
 
 void event_system_t::update_camera(camera_t& camera, GLfloat delta)
 {
-	float speed = 0.001;
+	float speed = 0.002;
 	// Handles key inputs
+
+	// TODO move_forward / backward / right / left + assert that move_on param is greater than 0
 	if (glfwGetKey(window_, GLFW_KEY_W) == GLFW_PRESS)
 	{
 		camera.move(speed * glm::vec3{ 0.f, 0.f, 1.f });
 	}
-
+	if (glfwGetKey(window_, GLFW_KEY_A) == GLFW_PRESS)
+	{
+		camera.move(speed * glm::vec3{ 1.f, 0.f, 0.f });
+	}
+	if (glfwGetKey(window_, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		camera.move(speed * -glm::vec3{ 0.f, 0.f, 1.f });
+	}
+	if (glfwGetKey(window_, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		camera.move(speed * -glm::vec3{ 1.f, 0.f, 0.f });
+	}
 }
